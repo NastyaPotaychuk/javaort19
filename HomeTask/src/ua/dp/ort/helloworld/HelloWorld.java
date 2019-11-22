8fcfd3f3c54677343f70e90ed5ae7a6129bb2f53
NastyaPotaychuk <57828615+NastyaPotaychuk@users.noreply.github.com>
2019-11-22 17:21:10 +0200

    Initial commit for home task project

--- HomeTask/src/ua/dp/ort/helloworld/HelloWorld.java
+++ HomeTask/src/ua/dp/ort/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package ua.dp.ort.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello World");
 
 	}
 
