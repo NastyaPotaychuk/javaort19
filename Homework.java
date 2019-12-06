a599f97f0d9e886b0f29654cd0124e862df68a1c
NastyaPotaychuk <57828615+NastyaPotaychuk@users.noreply.github.com>
2019-12-06 21:55:12 +0200

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,26 @@
+
+public class Homework {
+
+	public static void main(String[] args) {
+		
+//		int a = 5;
+//		int b = 10;
+		
+//		System.out.println(a+b);
+//		System.out.println(a-b);
+//		System.out.println(a*b);
+//		System.out.println((float)a/b);
+		
+		
+//		int c = 5;
+//		int d = 5;
+//		Math.pow(c, d);
+//		System.out.println(cd);
+		
+		int a = 1999999999;
+		int b = 1999999999;
+		
+		
+		
+	}
+}
