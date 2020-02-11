b28d86795484234ec22819f9ad69360199d6dedd
NastyaPotaychuk <57828615+NastyaPotaychuk@users.noreply.github.com>
2020-02-11 19:58:47 +0200

    Add files via upload
    Examination Task

---
+++ Examination.java
@@ -0,0 +1,16 @@
+
+public class Examination {
+
+	public static void main(String[] args) {
+	
+	int a [][] = new int [9][7];
+    for (int i = 0 ; i < a.length; i++) {
+	for (int j = 0 ; j < a[i].length; j++) {
+    System.out.print("* ");
+	}
+	System.out.println();
+	
+	}
+	}
+	}
+
